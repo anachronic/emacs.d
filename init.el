07afc72fa61d350061378f78139a717574d202c1
Nicolas Salas V <nikosalas@gmail.com>
2015-12-11 15:17:20 -0300

    Add moveline and rebinds

--- init.el
+++ init.el
@@ -6,12 +6,21 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
+;; todos los .el custom
+(add-to-list 'load-path "~/.emacs.d/elcustom/")
+
+;; moveline
+(require 'movelines)
+
 ;; visual.
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; magit conf
+;; rebind keys
 (global-set-key (kbd "<f8>") 'magit-status)
 (global-set-key (kbd "C-x g") 'magit-status)  ;; Legacy
+(global-set-key [C-tab] 'other-window)
+
+
