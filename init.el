554044678a74b34f54fe112807fc9213b45e1066
Nicolas Salas V <nikosalas@gmail.com>
2015-12-11 14:42:12 -0300

    Commit inicial

---
+++ init.el
@@ -0,0 +1,7 @@
+;; packages
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
