b154989485e7650a1447f878ee15a5322f33fe96
Nicolas Salas V <nikosalas@gmail.com>
2015-12-11 14:56:59 -0300

    Add visual y keybinds para magit

--- init.el
+++ init.el
@@ -5,3 +5,13 @@
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+
+;; visual.
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; magit conf
+(global-set-key (kbd "<f8>") 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)  ;; Legacy
