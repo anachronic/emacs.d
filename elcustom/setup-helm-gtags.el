70f9d953d6e7902a44d53d5cb627294c1a2c09b5
Nicolas Salas V <nikosalas@gmail.com>
2015-12-12 19:47:50 -0300

    Add jump to symbol binding and stdio snippet

--- elcustom/setup-helm-gtags.el
+++ elcustom/setup-helm-gtags.el
@@ -21,5 +21,6 @@
 (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
 (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
 (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+(define-key helm-gtags-mode-map (kbd "C-c C-z") 'helm-gtags-find-tag)
 
 (provide 'setup-helm-gtags)
