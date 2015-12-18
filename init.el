e13db7fe8bdf1231ca3381fba37bce6b4933c365
Nicolas Salas V <nikosalas@gmail.com>
2015-12-17 21:30:05 -0300

    Add shell-pop and typedef struct snippet.

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
 (require 'setup-markdown-mode)
 (require 'setup-powerline)
 (require 'highlight-lines)
+(require 'shell-pop)
 
 ;;;;;;;;;;;;;;;;;;;;;; configs
 (powerline-center-theme)
@@ -101,6 +102,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((company-clang-arguments "-I/home/nsalas/Projects/tarea3-algoritmos/src/")))))
+ '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
+ '(shell-pop-universal-key "<f5>")
+ '(shell-pop-window-position "right")
+ '(shell-pop-window-size 50))
