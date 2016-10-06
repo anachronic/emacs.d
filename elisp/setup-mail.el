3bb96f0ec374478bb2bc20d1bc4f4b7957154c50
Nicolas Salas V <nikosalas@gmail.com>
2016-10-05 21:12:01 -0300

    Add Mail config.

---
+++ elisp/setup-mail.el
@@ -0,0 +1,12 @@
+;;; setup-mail.el --- Mail conf file.
+;;; Commentary:
+;;; Code:
+
+(load-file "~/elisp/mail.el")
+;; (setq send-mail-function    'smtpmail-send-it
+;;       smtpmail-smtp-server  "example.org"
+;;       smtpmail-stream-type  'ssl
+;;       smtpmail-smtp-service 587)
+
+(provide 'setup-mail)
+;;; setup-mail.el ends here
