3d1d2e0aca06a7ab7e8f01a90039c5e038820c07
Nicolas Salas V <nikosalas@gmail.com>
2016-10-09 01:47:08 -0300

    Add async mail sending feature.

--- elisp/setup-mail.el
+++ elisp/setup-mail.el
@@ -5,12 +5,18 @@
 (when (file-exists-p "~/elisp/mail.el")
   (load-file "~/elisp/mail.el"))
 
-
+;; The following settings are ~/elisp/mail.el examples.
 ;; (setq user-full-name        "John Doe."
 ;;       user-mail-address     "example@example.org"
 ;;       smtpmail-smtp-server  "example.org"
 ;;       smtpmail-smtp-service 587
 ;;       send-mail-function    'smtpmail-send-it)
+
+
+;; We want to send mail asynchronously
+(require 'smtpmail-async)
+(setq send-mail-function 'async-smtpmail-send-it
+      message-send-mail-function 'async-smtpmail-send-it)
 
 
 (provide 'setup-mail)
