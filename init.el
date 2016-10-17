f832d11ebbf11073404a3b4f63f675f6f926a0b1
Nicolás Salas V <nikosalas@gmail.com>
2016-10-17 00:05:42 -0300

    Add bullet snippet and Droid Sans Mono font

--- init.el
+++ init.el
@@ -270,4 +270,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Droid Sans Mono for Powerline" :foundry "1ASC" :slant normal :weight normal :height 120 :width normal)))))
+
+;; just to suppress warnings.
+(provide 'init)
+;;; init.el ends here
