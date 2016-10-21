327f96dcc6607ae8a5112194286c004614fb0506
Nicolás Salas V <nikosalas@gmail.com>
2016-10-21 02:06:56 -0300

    Add Vala support

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; package --- Summary
+>;;; package --- Summary
 ;;; Commentary:
 ;;
 ;; I have nothing substantial to say here.
@@ -257,6 +257,15 @@
 
 ;; News reading conf
 (require 'setup-news)
+
+;; Vala mode setuo
+(require 'vala-mode)
+
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
 
 
 (custom-set-variables
