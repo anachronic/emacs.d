b88c5d210ccf84d710780701652f22adaae7ce97
Nicolas Salas V <nikosalas@gmail.com>
2016-10-22 00:55:44 -0300

    Add django mode and an init html snippet

--- elisp/python-programming.el
+++ elisp/python-programming.el
@@ -2,10 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;; elpy is GREAT!
 (use-package elpy
   :ensure t
   :config
   (elpy-enable))
+
+;; python-django seems to work quite ok with it.
+(use-package python-django
+  :ensure t)
 
 (provide 'python-programming)
 ;;; python-programming.el ends here
