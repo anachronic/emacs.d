bfeb1126b4f2e9b566b5e5f899c6209b30f3b4a6
Nicolás Salas V <nikosalas@gmail.com>
2016-10-27 01:29:14 -0300

    Setup org-bullets and header stuff

--- elisp/setup-org.el
+++ elisp/setup-org.el
@@ -1,5 +1,22 @@
+;;; setup-org.el --- Setup org mode in Emacs.
+;;; Commentary:
+;;; Code:
 
+;; Agenda is cool
 (global-set-key (kbd "C-c a") 'org-agenda)
+
+;; org keywords. I like having more than the usual TODO/
+(setq org-todo-keywords
+       '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELLED(c@)")))
+
+;; org-bullets bro!
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
+
 
 ;; Let's load our agenda file(s)
 (setq my/org-agenda-file "~/Dropbox/agenda.org")
