6993f1f04f812d752271534892876976becc17d5
Nicolas Salas V <nikosalas@gmail.com>
2016-11-07 19:07:25 -0300

    Add toggle fill/unfill paragraph

--- elisp/setup-editor.el
+++ elisp/setup-editor.el
@@ -339,5 +339,22 @@ point reaches the beginning or end of the buffer, stop there."
   :bind (("M-z"   . zzz-up-to-char)
          ("C-M-z" . zzz-to-char)))
 
+;; I found out this code snippet that while it doesn't really work in
+;; the scratch buffer, it does look useful. We shall see. Should be
+;; useful when reading long stuff URL:
+;; http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
 (provide 'setup-editor)
 ;;; setup-editor.el ends here
