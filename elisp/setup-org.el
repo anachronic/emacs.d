bac58610829b7b3a9771b1d21b283a175276b796
Nicolas Salas V <nikosalas@gmail.com>
2016-11-10 04:29:42 -0300

    Set up reveal.js for Org mode presentations

--- elisp/setup-org.el
+++ elisp/setup-org.el
@@ -7,7 +7,7 @@
 
 ;; org keywords. I like having more than the usual TODO/
 (setq org-todo-keywords
-       '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELLED(c@)")))
+      '((sequence "TODO(t)" "WAITING(w@/!)" "|" "DONE(d!)" "CANCELLED(c@)")))
 
 ;; org-bullets bro!
 (use-package org-bullets
@@ -15,16 +15,21 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-
-
-
 ;; Let's load our agenda file(s)
 (setq my/org-agenda-file "~/Dropbox/agenda.org")
 
 (when (file-exists-p my/org-agenda-file)
   (setq org-agenda-files (list my/org-agenda-file)))
 
+;; reveal.js stuff
+(use-package ox-reveal
+  :ensure ox-reveal
+  :config
+  (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+  (setq org-reveal-mathjax t))
 
+(use-package htmlize
+  :ensure t)
 
 (provide 'setup-org)
 ;;; setup-org.el ends here
