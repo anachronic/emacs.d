56072a983c27242f5be0ff824c00dc671cbb35c2
Nicolas Salas V <nikosalas@gmail.com>
2016-11-17 01:05:48 -0300

    Force color-identifiers-mode in python-mode

--- elisp/python-programming.el
+++ elisp/python-programming.el
@@ -17,5 +17,13 @@
 (use-package python-django
   :ensure t)
 
+;; For some reason color identifiers mode doesn't start with Python I
+;; guess either Python is not a prog-mode or Elpy doesn't play nice
+;; with it. Whatever
+(defun my/add-color-identifiers-mode ()
+  "Add color identifiers mode to the buffer or whatever."
+  (color-identifiers-mode 1))
+(add-hook 'python-mode-hook #'my/add-color-identifiers-mode)
+
 (provide 'python-programming)
 ;;; python-programming.el ends here
