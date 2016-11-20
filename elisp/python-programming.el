95192a6c353ad1cc493b092c2d3e0ee5c1238e6b
Nicolas Salas V <nikosalas@gmail.com>
2016-11-20 04:35:06 -0300

    Remove Elpy

--- elisp/python-programming.el
+++ elisp/python-programming.el
@@ -3,15 +3,7 @@
 ;;; Code:
 
 
-;; elpy is GREAT!
-(use-package elpy
-  :ensure t
-  :commands elpy-enable
-  :init (with-eval-after-load 'python (elpy-enable))
-  :config
-  (delete 'elpy-module-highlight-indentation elpy-modules)
-  (delete 'elpy-module-yasnippet elpy-modules)
-  (delete 'elpy-module-flymake elpy-modules))
+
 
 ;; python-django seems to work quite ok with it.
 (use-package python-django
