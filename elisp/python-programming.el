6ef14465d1da61c66c7aa65dd7b1b2da530cae42
Nicolas Salas V <nikosalas@gmail.com>
2016-11-20 06:27:10 -0300

    Add py-yapf and pyvenv to Python

--- elisp/python-programming.el
+++ elisp/python-programming.el
@@ -18,6 +18,28 @@
     (add-to-list 'company-backends 'company-anaconda))
   (add-hook 'python-mode-hook #'my/add-company-anaconda))
 
+;; Since linters are more clever than me at formatting code, let's use
+;; a tool for that, but let's not be too aggresive, just bind it to a
+;; key
+(use-package py-yapf
+  :ensure t
+  :config
+  (defun my/redef-indent-python ()
+    (local-set-key (kbd "C-c TAB") #'py-yapf-buffer))
+  (add-hook 'python-mode-hook #'my/redef-indent-python))
+
+;; I liked the pyvenv tool from Elpy. So let's use that. Also, I
+;; pretty much use (and will continue to do so) virtualenvwrapper.sh,
+;; Thus, the binding that will be used is pyvenv-workon in C-c C-w. We
+;; will also need a deactivate key, C-c C-d makes sense.
+(use-package pyvenv
+  :ensure t
+  :config
+  (defun my/set-pyvenv-workon ()
+    (local-set-key (kbd "C-c C-w") #'pyvenv-workon)
+    (local-set-key (kbd "C-c C-d") #'pyvenv-deactivate))
+  (add-hook 'python-mode-hook #'my/set-pyvenv-workon))
+
 ;; python-django seems to work quite ok with it.
 (use-package python-django
   :ensure t)
