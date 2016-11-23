545578230de9557aea6b647425d959f4df100ad5
Nicolas Salas V <nikosalas@gmail.com>
2016-11-23 19:46:09 -0300

    Add ibuffer tweaks. Including ibuffer-vc

--- elisp/setup-ui.el
+++ elisp/setup-ui.el
@@ -128,7 +128,52 @@ Otherwise exit Emacs."
 (global-set-key (kbd "M-n") 'my/scrolldown)
 (global-set-key (kbd "M-p") 'my/scrollup)
 
+;; I've been stealing from Steve Purcell's config a lot. So let's
+;; tweak ibuffer to show human readable sizes
+(with-eval-after-load 'ibuffer
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size))))))
 
+
+(with-eval-after-load 'ibuffer
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                filename-and-process)
+          (mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                filename-and-process))))
+
+;; Purcell's ibuffer-vc. I actually use a lot more helm-mini, but
+;; sometimes I get lost into how many buffers I open, so this can be
+;; handy in that situation.
+(use-package ibuffer-vc
+  :ensure t
+  :after ibuffer
+  :config
+  (require 'ibuffer-vc)
+  (defun ibuffer-set-up-preferred-filters ()
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'filename/process)
+      (ibuffer-do-sort-by-filename/process)))
+  (add-hook 'ibuffer-hook #'ibuffer-set-up-preferred-filters))
 
 (provide 'setup-ui)
 ;;; setup-ui.el ends here
