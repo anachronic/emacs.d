adc985422be713a2899aec946c28660497670e8f
Nicolas Salas V <nikosalas@gmail.com>
2016-11-23 21:43:19 -0300

    Use dired+. It's pretty cool.

--- elisp/setup-file-nav.el
+++ elisp/setup-file-nav.el
@@ -11,10 +11,6 @@
   :ensure t
   :bind ("<f7>" . neotree-toggle))
 
-;; Since b is unbound in dired and ^ is a very annoying key to press,
-;; lets bind that to dired-up-directory
-(define-key dired-mode-map (kbd "b") #'dired-up-directory)
-
 ;; This package is cool, I like coloring stuff around
 (use-package dired-k
   :ensure t
@@ -22,6 +18,17 @@
   (progn
     (add-hook 'dired-initial-position-hook 'dired-k)
     (add-hook 'dired-after-readin-hook #'dired-k-no-revert)))
+
+;; it seems sensible to hide every detail by default and use ) when I
+;; have to show more info, like file size or permissions
+(use-package dired+
+ :ensure t
+ :config
+ (global-dired-hide-details-mode 1)
+ ;; Since b is unbound in dired and ^ is a very annoying key to press,
+ ;; lets bind that to dired-up-directory
+ (with-eval-after-load 'dired+
+   (define-key dired-mode-map (kbd "b") #'dired-up-directory)))
 
 (provide 'setup-file-nav)
 ;;; setup-file-nav.el ends here
