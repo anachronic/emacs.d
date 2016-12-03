d141eb1d2fb184f3d524f317e72d4a61fb00a14b
Nicolas Salas V <nikosalas@gmail.com>
2016-12-03 05:15:29 -0300

    Use default web mode settings

--- elisp/web-programming.el
+++ elisp/web-programming.el
@@ -16,10 +16,7 @@
    ("\\.html?\\'" . web-mode))
   :config
   (progn
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 4)
     (setq web-mode-comment-style 2)
-    (setq web-mode-script-padding 0)
     (setq web-mode-enable-current-element-highlight t)
     (setq web-mode-engines-alist
           '(("django"    . "\\.html\\'"))
