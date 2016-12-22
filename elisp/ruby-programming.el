7fc9a5ac45eacf0055e585081d44e734b6bd83bf
Nicolas Salas V <nikosalas@gmail.com>
2016-12-22 02:23:05 -0300

    Set up ruby programming

---
+++ elisp/ruby-programming.el
@@ -0,0 +1,17 @@
+;;; ruby-programming.el --- Ruby related stuff.
+;;; Commentary:
+;;; Code:
+
+;; The first thing is to automatically add end to any starting
+;; sequence like def, if, while, do, etc.
+(use-package ruby-end
+  :ensure t
+  :config
+  (setq ruby-end-insert-newline nil))
+
+;; Ruby is one of those annoying 2-space indent languages. Let's use a
+;; guide
+(add-hook 'ruby-mode-hook #'indent-guide-mode)
+
+(provide 'ruby-programming)
+;;; ruby-programming.el ends here
