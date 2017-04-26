050bfb72f24be13c8a84bed05f5b7c5d893077a0
Nicolas Salas V <nikosalas@gmail.com>
2017-04-26 02:37:13 -0300

    Drop notmuch in favor of mu4e

--- elisp/setup-mail.el
+++ elisp/setup-mail.el
@@ -11,7 +11,48 @@
 ;;       smtpmail-smtp-service 587
 ;;       send-mail-function    'smtpmail-send-it)
 
+(require 'mu4e)
 
+;; default
+(setq mu4e-maildir (expand-file-name "~/Mail"))
+
+(setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+;; don't save message to Sent Messages, GMail/IMAP will take care of this
+(setq mu4e-sent-messages-behavior 'delete)
+
+;; setup some handy shortcuts
+(setq mu4e-maildir-shortcuts
+      '(("/INBOX"             . ?i)
+        ("/[Gmail].Sent Mail" . ?s)
+        ("/[Gmail].Trash"     . ?t)))
+
+;; allow for updating mail using 'U' in the main view:
+;; (setq mu4e-get-mail-command "offlineimap")
+
+(setq message-citation-line-format "On %D %I:%M %p, %N wrote:"
+      mail-from-style 'angles
+      message-cite-style 'message-cite-style-gmail
+      message-citation-line-function 'message-insert-formatted-citation-line)
+
+;; sending mail -- replace USERNAME with your gmail username
+;; also, make sure the gnutls command line utils are installed
+;; package 'gnutls-bin' in Debian/Ubuntu, 'gnutls' in Archlinux.
+
+(require 'smtpmail)
+
+(setq message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials
+      '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials
+      (expand-file-name "~/.authinfo.gpg")
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-debug-info t)
 
 (provide 'setup-mail)
 ;;; setup-mail.el ends here
