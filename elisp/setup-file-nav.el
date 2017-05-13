d22e6f9d7209e5a0a01aa392c684c27cb8221818
Nicolas Salas V <nikosalas@gmail.com>
2017-05-13 20:13:03 -0300

    Use isearch instead of dired-narrow

--- elisp/setup-file-nav.el
+++ elisp/setup-file-nav.el
@@ -41,16 +41,8 @@
   ;; Since b is unbound in dired and ^ is a very annoying key to press,
   ;; lets bind that to dired-up-directory
   (with-eval-after-load 'dired+
-    (define-key dired-mode-map (kbd "b") #'dired-up-directory)))
-
-;; I've found out that I often need to narrow/filter dired buffers. So
-;; let's do something about it
-(use-package dired-narrow
-  :ensure t
-  :bind (:map dired-mode-map
-              ("/" . dired-narrow))
-  :config
-  (define-key dired-mode-map (kbd "C-S-s") #'dired-narrow))
+    (define-key dired-mode-map (kbd "b") #'dired-up-directory)
+    (define-key dired-mode-map (kbd "/") #'isearch)))
 
 ;; We want to be able to toggle dot files in dired
 (add-hook 'dired-mode-hook (lambda ()
