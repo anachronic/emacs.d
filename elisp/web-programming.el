acb898421099f1425e951255a850f618ab7747c2
Nicolás Salas V <nikosalas@gmail.com>
2017-05-18 15:07:59 -0400

    Fix padding in web mode for style and script tags

--- elisp/web-programming.el
+++ elisp/web-programming.el
@@ -20,6 +20,8 @@
     (setq web-mode-enable-current-element-highlight t)
     (setq web-mode-markup-indent-offset 2)
     (setq web-mode-code-indent-offset 2)
+    (setq web-mode-style-padding 2)
+    (setq web-mode-script-padding 2)
     (setq web-mode-engines-alist
           '(("django"    . "\\.html\\'"))
           )))
