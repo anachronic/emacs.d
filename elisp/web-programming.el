1caa2b1d3e898fd13641eb17d7f01de3a9144345
Nicolás Salas V <nikosalas@gmail.com>
2017-05-29 11:29:30 -0400

    Set web-mode-css-indent-offset to 2 globally

--- elisp/web-programming.el
+++ elisp/web-programming.el
@@ -20,6 +20,7 @@
     (setq web-mode-enable-current-element-highlight t)
     (setq web-mode-markup-indent-offset 2)
     (setq web-mode-code-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
     (setq web-mode-style-padding 2)
     (setq web-mode-script-padding 2)
     (setq web-mode-engines-alist
