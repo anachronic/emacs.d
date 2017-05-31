fc1605ed4e04da6222a621b81a0efb68555b3808
Nicolas Salas V <nikosalas@gmail.com>
2017-05-30 22:28:44 -0400

    Remove color-identifiers from js2 and remove jsx from auto modes

--- elisp/js-programming.el
+++ elisp/js-programming.el
@@ -6,14 +6,13 @@
   :ensure t
   :commands (js2-mode js2-jsx-mode)
   :defer t
-  :mode (("\\.js\\'" . js2-mode)
-         ("\\.jsx?\\'" . js2-jsx-mode))
+  :mode (("\\.js\\'" . js2-mode))
   :config
   (setq js2-basic-offset 2)
   (setq-default js2-mode-show-parse-errors nil
                 js2-mode-show-strict-warnings nil
-                js2-idle-timer-delay 0.6)
-  (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode)))
+                js2-idle-timer-delay 0.4)
+  (add-hook 'js2-mode-hook (lambda () (color-identifiers-mode -1))))
 
 (use-package tern
   :ensure t
