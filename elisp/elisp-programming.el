3797c7bed3eee21675ba7a81fa97b802343901a7
Nicolás Salas V <nikosalas@gmail.com>
2017-06-06 12:10:01 -0400

    Make paredit and smartparens compatible with hungry-delete-mode

--- elisp/elisp-programming.el
+++ elisp/elisp-programming.el
@@ -13,13 +13,14 @@
 ;; I like prettify symbols mode. but only for elisp
 (add-hook 'emacs-lisp-mode-hook 'prettify-symbols-mode)
 
-;; I'll be trying Paredit. Should be quite useful for lisp-like stuff
-;; Yeah, I've been becoming more fond of paredit every day. It rocks!
+;; Paredit is the best package ever.
 (use-package paredit
   :ensure t
   :diminish "par"
   :init
   (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
   :config
   ;; First remove some unused keys
   (define-key paredit-mode-map (kbd "C-<backspace>") 'paredit-backward-kill-word)
@@ -28,12 +29,31 @@
   (define-key paredit-mode-map (kbd "C-<right>") nil)
   (define-key paredit-mode-map (kbd "M-?") nil)
 
-  ;; Use paredit
-  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
-
   ;; This thing is so freaking useful it freaks me out a little.
-  (global-set-key (kbd "M-K") #'paredit-kill))
+  (global-set-key (kbd "M-K") #'paredit-kill)
+
+  ;; Try to make it play well with hungry delete
+  ;; forward delete
+  (defvar ach-whitespace-eol-re "\\(\\s-\\|$\\)+")
+  (defvar ach-whitespace-bol-re "\\(\\s-\\|^\\)+")
+  (defun paredit-forward-hungry-delete (orig-fun &rest argz)
+    (if (not (looking-at ach-whitespace-eol-re))
+        (apply orig-fun argz)
+      (unless (car argz)
+        (setq argz '(1)))
+      (apply 'hungry-delete-forward argz)))
+
+  (advice-add 'paredit-forward-delete :around #'paredit-forward-hungry-delete)
+
+  (defun paredit-backward-hungry-delete (orig-fun &rest argz)
+    (if (not (looking-back ach-whitespace-bol-re))
+        (apply orig-fun argz)
+      (unless (car argz)
+        (setq argz '(1)))
+      (apply 'hungry-delete-backward argz)))
+
+  (advice-add 'paredit-backward-delete :around #'paredit-backward-hungry-delete)
+  )
 
 ;; Stealing conf from purcell's .emacs.d. This package is actually
 ;; pretty cool, it gets you out of the dullness of full white text (or
--- elisp/setup-editor.el
+++ elisp/setup-editor.el
@@ -518,6 +518,29 @@ Single Capitals as you type."
   (setq sp-highlight-pair-overlay nil)
   (setq sp-highlight-wrap-overlay nil)
   (setq sp-highlight-wrap-tag-overlay nil)
+
+
+  ;; Make it compatible with hungry-delete-mode
+  ;; src: https://emacs.stackexchange.com/a/29484
+  (dolist (key '( [remap delete-char]
+                  [remap delete-forward-char]))
+
+    (define-key smartparens-strict-mode-map key
+      '(menu-item "maybe-sp-delete-char" nil
+                  :filter (lambda (&optional _)
+                            (unless (looking-at-p "[[:space:]\n]")
+                              #'sp-delete-char)))))
+
+
+  (dolist (key '([remap backward-delete-char-untabify]
+                 [remap backward-delete-char]
+                 [remap delete-backward-char]))
+
+    (define-key smartparens-strict-mode-map key
+      '(menu-item "maybe-sp-backward-delete-char" nil
+                  :filter (lambda (&optional _)
+                            (unless (looking-back "[[:space:]\n]" 1)
+                              #'sp-backward-delete-char)))))
   )
 
 ;; Hungry delete mode seems very good
