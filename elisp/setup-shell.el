9037b5dc96963f04b0e7c9c1f5ad618a32ea51b2
Nicolas Salas V <nikosalas@gmail.com>
2017-07-01 06:57:55 -0400

    Give virtual env support to eshell

--- elisp/setup-shell.el
+++ elisp/setup-shell.el
@@ -68,15 +68,22 @@
       (list (file-name-directory directory) (file-name-base directory))
     (list "" directory)))
 
+(defun ach--pyvenv-prompt ()
+  "Get virtualenv prompt if its running."
+  (when (bound-and-true-p pyvenv-virtual-env-name)
+    (concat "(" pyvenv-virtual-env-name ") ")))
+
 (setq eshell-prompt-function
       (lambda ()
         (let* ((directory (split-directory-prompt (pwd-shorten-dirs (pwd-replace-home (eshell/pwd)))))
                (parent (car directory))
                (name (cadr directory))
-               (branch (or (curr-dir-git-branch-string (eshell/pwd)) "")))
+               (branch (or (curr-dir-git-branch-string (eshell/pwd)) ""))
+               (venv (or (ach--pyvenv-prompt) "")))
 
           (if (eq 'dark (frame-parameter nil 'background-mode))
               (concat   ;; Prompt for Dark Themes
+               (propertize venv 'face `(:foreground "#8888FF"))
                (propertize parent 'face `(:foreground "#8888FF"))
                (propertize name   'face `(:foreground "#8888FF" :weight bold))
                (propertize branch 'face `(:foreground "green"))
@@ -84,6 +91,7 @@
                (propertize " "    'face `(:weight bold)))
 
             (concat    ;; Prompt for Light Themes
+             (propertize venv   'face `(:foreground "#000000"))
              (propertize parent 'face `(:foreground "blue"))
              (propertize name   'face `(:foreground "blue" :weight bold))
              (propertize branch 'face `(:foreground "dark green"))
@@ -93,7 +101,6 @@
 (setq eshell-highlight-prompt nil)
 
 ;; ---------------------------------- END HA's copy ------------------------------
-;; well, aliases go in eshell/ directory, so i won't bother with that for this.
 
 ;; I use C-d quite frequently when in shells. I guess I want that
 ;; too. Thank you Howard!
