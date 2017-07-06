991f2c9f734746910a21132ce00cde435f85c5f8
Nicolas Salas V <nikosalas@gmail.com>
2017-07-05 22:31:16 -0400

    Use edbi for databases

---
+++ elisp/init-databases.el
@@ -0,0 +1,13 @@
+;;; init-databases.el --- Database handling.
+;;; Commentary:
+;;; Code:
+
+;; EDBI is something i've wanted for a long time. I thought there was
+;; no such thing, but i guess i was wrong. Nice
+(use-package edbi
+  :ensure t
+  :defer t
+  )
+
+(provide 'init-databases)
+;;; init-databases.el ends here
