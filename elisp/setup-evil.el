6150445ea200c8d64d813e511f7e593c49c4a1fa
Nicolás Salas V <nikosalas@gmail.com>
2018-04-09 00:41:29 -0300

    Add evil-leader

--- elisp/setup-evil.el
+++ elisp/setup-evil.el
@@ -2,10 +2,36 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package evil-leader
+  :demand t
+  :ensure t
+  :commands (global-evil-leader-mode))
+
 (use-package evil
   :commands (evil-mode evil-define-key)
+  :ensure t
   :demand t
+  :init
+  (global-evil-leader-mode)
   :config
+  (evil-leader/set-leader "<SPC>")
+  (evil-define-key 'normal global-map (kbd "C-p") 'counsel-projectile-find-file)
+  (evil-leader/set-key
+    "<SPC>" 'counsel-M-x
+
+    ;; buffers
+    "bb" 'ivy-switch-buffer
+
+    ;; misc
+    "g" 'ffap
+
+    ;; rg
+    "a" 'counsel-projectile-rg
+
+    ;; projectile
+    "pp" 'projectile-switch-project
+    "pf" 'counsel-projectile-find-file
+    )
   (evil-mode 1))
 
 (provide 'setup-evil)
