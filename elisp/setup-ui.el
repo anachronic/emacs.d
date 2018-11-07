a1074c72d050aa734b4f43ac237987c065ff7575
Nicolás Salas V <nikosalas@gmail.com>
2018-11-07 17:11:38 -0300

    Tweak eyebrowse config and remove transpose-windows
    Eyebrowse can now use :tabnew, :tabclose, zk to kill.
    Don't use transpose windows anymore, use C-w C-r instead

--- elisp/setup-ui.el
+++ elisp/setup-ui.el
@@ -36,9 +36,9 @@
   :ensure t
   :init
   (setq-default eyebrowse-keymap-prefix (kbd "C-x x"))
-  (setq-default eyebrowse-mode-line-style 'always)
-  :config
   (eyebrowse-mode)
+  :config
+  ;; opinionated keys
   (define-key eyebrowse-mode-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
   (define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
   (define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
@@ -52,25 +52,11 @@
   (define-key evil-motion-state-map (kbd "gt") 'eyebrowse-next-window-config)
   (define-key evil-motion-state-map (kbd "gT") 'eyebrowse-prev-window-config)
   (define-key evil-motion-state-map (kbd "C-SPC") 'eyebrowse-last-window-config)
-  (define-key evil-motion-state-map (kbd "zx") 'eyebrowse-last-window-config))
-
-;; From https://www.emacswiki.org/emacs/TransposeWindows
-;; and crux.
-(defun transpose-windows (arg)
-  "Transpose the buffers shown in two windows.
-
-Prefix ARG determines if the current windows buffer is swapped
-with the next or previous window, and the number of
-transpositions to execute in sequence."
-  (interactive "p")
-  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
-    (while (/= arg 0)
-      (let ((this-win (window-buffer))
-            (next-win (window-buffer (funcall selector))))
-        (set-window-buffer (selected-window) next-win)
-        (set-window-buffer (funcall selector) this-win)
-        (select-window (funcall selector)))
-      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+  (define-key evil-motion-state-map (kbd "zk") 'eyebrowse-close-window-config)
+  (define-key evil-motion-state-map (kbd "zx") 'eyebrowse-last-window-config)
+  (evil-ex-define-cmd "tabn[ew]" 'eyebrowse-create-window-config)
+  (evil-ex-define-cmd "tabc[lose]" 'eyebrowse-close-window-config)
+  )
 
 (provide 'setup-ui)
 ;;; setup-ui.el ends here
