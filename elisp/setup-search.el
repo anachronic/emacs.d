47d9b23b3d23b9eaadd2a93f613169f078b3ff16
Nicolás Salas V <nikosalas@gmail.com>
2018-11-07 14:34:02 -0300

    Use a more evil-ish approach to searches and fix dumb-jump
    Dumb jump needed some config as it is way better than "intellisense"
    packages for a lot of languages. Especially interpreted ones

--- elisp/setup-search.el
+++ elisp/setup-search.el
@@ -16,40 +16,27 @@
 ;; Lets use swiper conservatively. I actually like isearch better.
 (use-package swiper
   :ensure t
-  :bind (("C-c s" . swiper)
-         ("C-S-s" . swiper))
-  :config
-  ;; I'd like to do something along the lines of isearch-swiper, which
-  ;; means to switch my isearch query to swiper
-  (defun isearch-swiper ()
-    "Switch from isearch to swiper keeping query string."
-    (interactive)
-    (isearch-exit)
-    (anzu--reset-mode-line)
-    (swiper isearch-string))
-  (define-key isearch-mode-map (kbd "C-S-s") #'isearch-swiper)
-  (define-key isearch-mode-map (kbd "C-S-r") #'isearch-swiper)
-  (define-key isearch-mode-map (kbd "C-c s") #'isearch-swiper))
+  :init
+  (define-key evil-normal-state-map (kbd "C-s") 'swiper))
 
 ;; evil-anzu
 (use-package evil-anzu
   :ensure t
   :defer t
   :init
+  (setq-default anzu-cons-mode-line-p nil)
   (with-eval-after-load 'evil
     (require 'evil-anzu)))
 
 ;; This is GREAT when tags don't really cut it
 (use-package dumb-jump
   :ensure t
-  :diminish (dumb-jump-mode . "Dumb")
   :config
+  (setq dumb-jump-selector 'ivy)
+  (setq dumb-jump-prefer-searcher 'rg)
   (define-key dumb-jump-mode-map (kbd "C-M-g") nil)
   (define-key dumb-jump-mode-map (kbd "C-M-p") nil)
   (define-key dumb-jump-mode-map (kbd "C-M-q") nil)
-  (define-key dumb-jump-mode-map (kbd "M-g j") 'dumb-jump-go)
-  (define-key dumb-jump-mode-map (kbd "M-g b") 'dumb-jump-back)
-  (define-key dumb-jump-mode-map (kbd "M-g q") 'dumb-jump-quick-look)
   (add-hook 'prog-mode-hook 'dumb-jump-mode))
 
 (provide 'setup-search)
